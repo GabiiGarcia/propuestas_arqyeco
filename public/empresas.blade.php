5582eeef762c21bcca14477854479b629e724163
Gabii Albino <gabiialdacogarcia16@gmail.com>
2021-07-06 23:52:19 -0500

    se arreglo el documento empresas

--- public/empresas.blade.php
+++ public/empresas.blade.php
@@ -1,161 +1,161 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Arquitectura y urbanización ecológica</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
-<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
-<script src="js/cufon-yui.js" type="text/javascript"></script>
-<script src="js/cufon-replace.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
-<script src="js/FF-cash.js" type="text/javascript"></script>
+  <title>Arquitectura y urbanización ecológica</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+  <script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+  <script src="js/cufon-yui.js" type="text/javascript"></script>
+  <script src="js/cufon-replace.js" type="text/javascript"></script>
+  <script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
+  <script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
+  <script src="js/FF-cash.js" type="text/javascript"></script>
 <!--[if lt IE 9]>
 <script type="text/javascript" src="js/html5.js"></script>
 <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
 <![endif]-->
 </head>
 <body id="page3">
-<!--==============================header=================================-->
-<header>
-  <div class="menu-row">
-    <div class="main">
-      <nav class="wrapper">
-<ul class="menu">
-          <li><a class="active" href="index.html">Inicio</a></li>
-          <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
-          <li><a href="#">Arquitectura y urbanización ecologica</a>
-                                                        <ul>
-                                                            <li><a href="arqyeco.blade.php">¿Que es?</a></li>
-                                                            <li><a href="principios.blade.php">Principio</a></li>
-                                                            <li><a href="empresas.blade.php">Proyectos</a></li>
-
-                                                        </ul>
-                                                    </li> 
-          <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
-          <li><a href="propuestas.blade.php">Propuestas</a></li>
-          <li><a href="ventajas.blade.php">Ventajas</a></li>
-          <li><a href="contacto.blade.php">Contacto</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <div class="row-bot">
-    <div class="row-bot-bg">
+  <!--==============================header=================================-->
+  <header>
+    <div class="menu-row">
       <div class="main">
-        <div class="wrapper">
-          <h1><a href="index.html">Inicio</a></h1>
+        <nav class="wrapper">
+          <ul class="menu">
+            <li><a class="active" href="index.html">Inicio</a></li>
+            <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
+            <li><a href="#">Arquitectura y urbanización ecologica</a>
+              <ul>
+                <li><a href="arqyeco.blade.php">¿Que es?</a></li>
+                <li><a href="principios.blade.php">Principio</a></li>
+                <li><a href="empresas.blade.php">Proyectos</a></li>
+
+              </ul>
+            </li> 
+            <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
+            <li><a href="propuestas.blade.php">Propuestas</a></li>
+            <li><a href="ventajas.blade.php">Ventajas</a></li>
+            <li><a href="contacto.blade.php">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="row-bot">
+      <div class="row-bot-bg">
+        <div class="main">
+          <div class="wrapper">
+            <h1><a href="index.html">Inicio</a></h1>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</header>
-<!--==============================content================================-->
-<section id="content">
-  <div class="main">
-    <div class="container_12">
-      <div class="wrapper">
-        <article class="grid_3">
-          <h3 class="p1">Articulos relevantes</h3>
+  </header>
+  <!--==============================content================================-->
+  <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+          <article class="grid_3">
+            <h3 class="p1">Articulos relevantes</h3>
             <ul class="list-1">
-                        <li><a class="active" href="index.html">Inicio</a></li>
-          <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
-          <li><a href="#">Arquitectura y urbanizacion ecologica</a>
-                                                        <ul>
-                                                            <li><a href="arqyeco.blade.php">¿Que es?</a></li>
-                                                            <li><a href="principios.blade.php">Principio</a></li>
-                                                            <li><a href="empresas.blade.php">Empresas</a></li>
+              <li><a class="active" href="index.html">Inicio</a></li>
+              <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
+              <li><a href="#">Arquitectura y urbanizacion ecologica</a>
+                <ul>
+                  <li><a href="arqyeco.blade.php">¿Que es?</a></li>
+                  <li><a href="principios.blade.php">Principio</a></li>
+                  <li><a href="empresas.blade.php">Empresas</a></li>
 
-                                                        </ul>
-                                                    </li> 
-          <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
-          <li><a href="propuestas.blade.php">Propuestas</a></li>
-          <li><a href="ventajas.blade.php">Ventajas</a></li>
-          <li><a href="contacto.blade.php">Contacto</a></li>
-          </ul>
-        </article>
-        <article class="grid_9">
-          <FONT color="red"><h3>Proyectos</h3></FONT>
-          <div class="wrapper indent-bot">
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/edificio_eni.jpg" with="200px" height="200px"  alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Edificio ENI (Roma, Italia)</h6>
+                </ul>
+              </li> 
+              <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
+              <li><a href="propuestas.blade.php">Propuestas</a></li>
+              <li><a href="ventajas.blade.php">Ventajas</a></li>
+              <li><a href="contacto.blade.php">Contacto</a></li>
+            </ul>
+          </article>
+          <article class="grid_9">
+            <FONT color="red"><h3>Proyectos</h3></FONT>
+            <div class="wrapper indent-bot">
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/edificio_eni.jpg" with="200px" height="200px"  alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Edificio ENI (Roma, Italia)</h6>
               El proyecto es el resultado de una transformación radical de la sede actual de la empresa petroquímica ENI situada en las afueras de Roma. Se caracteriza por el rediseño de las fachadas orientadas al este y al oeste, que confieren al edificio la imagen de un verdadero "jardín vertical" de veinte pisos de altura.</div>
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/edificio_acros.jpg" with="200px" height="200px"  alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Edificio Acros (Fukuoka, Japón)</h6>
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/edificio_acros.jpg" with="200px" height="200px"  alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Edificio Acros (Fukuoka, Japón)</h6>
               Este proyecto combina el uso rentable del suelo con la necesidad ciudadana de espacios verdes abiertos. El edificio, de 60 metros de altura, alberga en su cubierta un sistema de terrazas que configuran un parque. Esta cubierta verde, además de oxigenar la ciudad, conserva la temperatura del edificio manteniéndola a niveles constantes; lo que incide en un bajo consumo energético.  </div>
-            <div class="col-2">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/viviendas100k.jpg" with="200px" height="200px" alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Viviendas 100 K (Prototipo)</h6>
+              <div class="col-2">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/viviendas100k.jpg" with="200px" height="200px" alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Viviendas 100 K (Prototipo)</h6>
               Presenta un diseño residencial con tres características esenciales: bajo precio, materiales de alta calidad y cero emisiones de CO2, sin olvidar un diseño realmente atractivo. Produce energía a través de una serie de estrategias que la convierten en una máquina bioclimática eficiente.</div>
-          </div>
-          <div class="wrapper indent-bot">
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><center><img src="images/edificio_cset.jpg" with="200px" height="200px" alt=""></center></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Edificio CSET (Ningbo, China)</h6>
+            </div>
+            <div class="wrapper indent-bot">
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><center><img src="images/edificio_cset.jpg" with="200px" height="200px" alt=""></center></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Edificio CSET (Ningbo, China)</h6>
               El Centro de Tecnologías Sustentables (Centre for Sustainable Energy Technologies) es el primer edificio con cero emisiones en China y su diseño está inspirado en las linternas típicas del país, utilizando pantallas de madera tradicionales, abanicos y pantallas articuladas con vidrio laminado, que varían la apariencia del edificio del día a la noche. </div>
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/masdar_city.jpg" with="200px" height="200px" alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Masdar City (Abu Dhabi)</h6>
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/masdar_city.jpg" with="200px" height="200px" alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Masdar City (Abu Dhabi)</h6>
               El proyecto para la ciudad de Masdar supone una colonización del desierto. Su nombre significa fuente y manantial. Situada a 15 km de Abu Dhabi, será la primera ciudad autosuficiente y libre de CO2. Todo gira en torno a las energías renovables. La ciudad se desarrolla bajo tierra a través de laberintos que juegan con la incidencia de la luz y la ventilación.</div>
-            <div class="col-2">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/free-university.jpg" with="200px" height="200px" alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Free University Berlin (Alemania) </h6>
+              <div class="col-2">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/free-university.jpg" with="200px" height="200px" alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Free University Berlin (Alemania) </h6>
               Curiosamente la biblioteca de esta universidad es conocida como “el cráneo de Berlín” debido a la forma que presenta, El proyecto abarca la restauración de los edificios modernistas y la creación de una nueva biblioteca para la facultad de Filología. </div>
-          </div>
-          <div class="wrapper">
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/geada-2055.jpg" with="200px" height="200px" alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">GEODA 2055 (Mondragón, Guipúzcoa)</h6>
+            </div>
+            <div class="wrapper">
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/geada-2055.jpg" with="200px" height="200px" alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">GEODA 2055 (Mondragón, Guipúzcoa)</h6>
               El propósito del proyecto es ampliar la ciudad de Mondragón sobre una cantera adyacente y al mismo tiempo recuperar el ecosistema degradado. El conjunto se asemeja a una “geoda” gigantesca y cada cubo parece uno de sus cristales asomándose al exterior.</div>
-            <div class="col-1">
-              <div class="p2">
-                <figure class="img-border"><a href="#"><img src="images/genesis-center.jpg" with="200px" height="200px" alt=""></a></figure>
-                <div class="clear"></div>
-              </div>
-              <h6 class="p1">Génesis Center (Taunton, Reino Unido) </h6>
+              <div class="col-1">
+                <div class="p2">
+                  <figure class="img-border"><a href="#"><img src="images/genesis-center.jpg" with="200px" height="200px" alt=""></a></figure>
+                  <div class="clear"></div>
+                </div>
+                <h6 class="p1">Génesis Center (Taunton, Reino Unido) </h6>
               El edificio es un centro de conferencias en pleno funcionamiento que supone un auténtico muestrario de técnicas y materiales de construcción sostenible. El proyecto, que ofrece un acercamiento integral a lo sostenible, pretende enseñar y educar sobre una nueva forma de pensar, más osada en cuanto al diseño ecológico contemporáneo. </div>
-          </div>
-        </article>
+            </div>
+          </article>
+        </div>
       </div>
     </div>
-  </div>
-</section>
-<!--==============================footer=================================-->
-<footer>
-  <div class="main">
-    <div class="aligncenter">
-      <p class="p0"> ArqyEco</p>
-      Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
-      <a href="#" >propuestasarqyeco@gmail.com</a>
-      <img src="images/maps-and-flags.png" width="10dp" height="15dp">
-     <a>Maravatio, Michoacan</a></div>
-  </div>
-</footer>
-<script type="text/javascript">Cufon.now();</script>
-</body>
-</html>
+  </section>
+  <!--==============================footer=================================-->
+  <footer>
+    <div class="main">
+      <div class="aligncenter">
+        <p class="p0"> ArqyEco</p>
+        Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
+        <a href="#" >propuestasarqyeco@gmail.com</a>
+        <img src="images/maps-and-flags.png" width="10dp" height="15dp">
+        <a>Maravatio, Michoacan</a></div>
+      </div>
+    </footer>
+    <script type="text/javascript">Cufon.now();</script>
+  </body>
+  </html>
