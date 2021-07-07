3795db9df26f9978df5cacecd87f4a5f4a27212f
Gabii Albino <gabiialdacogarcia16@gmail.com>
2021-07-06 23:54:14 -0500

    se reindento el archi index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,182 +1,182 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Arquitectura y urbanización ecológica</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
-<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
-<script src="js/cufon-yui.js" type="text/javascript"></script>
-<script src="js/cufon-replace.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
-<script src="js/FF-cash.js" type="text/javascript"></script>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/tms-0.3.js"></script>
-<script type="text/javascript" src="js/tms_presets.js"></script>
-<script src="js/jcarousellite_1.0.1.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function () {
-    $('.carousel .jCarouselLite').jCarouselLite({
-        btnNext: '.carousel .next',
-        btnPrev: '.carousel .prev',
-        speed: 600,
-        easing: 'easeOutQuart',
-        vertical: false,
-        circular: false,
-        visible: 4,
-        start: 0,
-        scroll: 1
-    });
-});
-</script>
+	<title>Arquitectura y urbanización ecológica</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+	<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+	<script src="js/cufon-yui.js" type="text/javascript"></script>
+	<script src="js/cufon-replace.js" type="text/javascript"></script>
+	<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
+	<script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
+	<script src="js/FF-cash.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/tms-0.3.js"></script>
+	<script type="text/javascript" src="js/tms_presets.js"></script>
+	<script src="js/jcarousellite_1.0.1.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('.carousel .jCarouselLite').jCarouselLite({
+				btnNext: '.carousel .next',
+				btnPrev: '.carousel .prev',
+				speed: 600,
+				easing: 'easeOutQuart',
+				vertical: false,
+				circular: false,
+				visible: 4,
+				start: 0,
+				scroll: 1
+			});
+		});
+	</script>
 <!--[if lt IE 9]>
 <script type="text/javascript" src="js/html5.js"></script>
 <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
 <![endif]-->
 </head>
 <body id="page1">
-<!--==============================header=================================-->
-<header>
-  <div class="menu-row">
-    <div class="main">
-      <nav class="wrapper">
-        <ul class="menu">
-                    <li><a class="active" href="index.blade.php">Inicio</a></li>
-          <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
-          <li><a href="#">Arquitectura y urbanizacion ecologica</a>
-                                                        <ul>
-                                                            <li><a href="arqyeco.blade.php">¿Que es?</a></li>
-                                                            <li><a href="principios.blade.php">Principio</a></li>
-                                                            <li><a href="empresas.blade.php">Empresas</a></li>
+	<!--==============================header=================================-->
+	<header>
+		<div class="menu-row">
+			<div class="main">
+				<nav class="wrapper">
+					<ul class="menu">
+						<li><a class="active" href="index.blade.php">Inicio</a></li>
+						<li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
+						<li><a href="#">Arquitectura y urbanizacion ecologica</a>
+							<ul>
+								<li><a href="arqyeco.blade.php">¿Que es?</a></li>
+								<li><a href="principios.blade.php">Principio</a></li>
+								<li><a href="empresas.blade.php">Empresas</a></li>
 
-                                                        </ul>
-                                                    </li> 
-          <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
-          <li><a href="propuestas.blade.php">Propuestas</a></li>
-          <li><a href="ventajas.blade.php">Ventajas</a></li>
-          <li><a href="contacto.blade.php">Contacto</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <div class="row-bot">
-    <div class="row-bot-bg">
-      <div class="main">
-        <div class="wrapper p6">
-          <h1><a href="index.html">Propuestas de arquitectura y urbanización ecologica</a></h1>
-          <h2>Conocenos!</h2>
-        </div>
-        <div class="slider-wrapper">
-          <div class="slider">
-            <ul class="items">
-              <li> <img src="images/slider1.jpg" alt=""> </li>
-              <li> <img src="images/slider2.jpg" alt=""> </li>
-              <li> <img src="images/slider3.jpg" alt=""> </li>            
-          </ul>
-          </div>
-        </div>
-        <div class="wrapper">
-          <figure class="img-indent2"><img src="images/page1-img1.png" alt=""></figure>
-          <div class="extra-wrap"> <strong class="text-1">El calentamiento global es uno (y no el más grave) de los principales problemas ecológicos. Es importante señalar la degradación de hábitats por contaminación, urbanismo y la problemática con las llamadas “especies invasoras”. Porello ayudemos nuestro planeta Tierra comenzando por nuestro hogar
-          <br>  
-          "La naturaleza se hace paisaje cuando el hombre la enmarca"</strong> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-<!--==============================aside================================-->
-<aside>
-  <div class="carousel"> <a class="prev"></a> <a class="next"></a>
-    <div class="carousel-container">
-      <h3>¡Bienvenido!</h3>
-      <div class="jCarouselLite">
-        <ul class="carousel-list">
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img2.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img3.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img4.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img5.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img6.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img7.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img8.jpg" alt="" /></a> </div>
-          </li>
-          <li>
-            <div class="img-border"> <a href="#"><img src="images/page1-img9.jpg" alt="" /></a> </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</aside>
-<!--==============================content================================-->
-<section id="content">
-  <div class="main">
-    <div class="container_12">
-      <div class="wrapper">
-        <article class="grid_4">
-          <h3 class="prev-indent-bot">Arquitectura y ecologia</h3>
-          <p class="img-indent-bot">«Yo hago lo que usted no puede, y usted hace lo que yo no puedo. Juntos podemos hacer grandes cosas». <br> Madre Teresa de Calcuta.</p>
-          <a class="button" href="#"><span>Conocer más</span></a> </article>
-        <article class="grid_4">
-          <div class="indent-left">
-            <h3 class="p1">Articulos relevantes</h3>
-            <ul class="list-1">
-              <li><a href="#">Proyecto ecologicó en América</a></li>
-              <li><a href="#">Implementa áreas verdes desde casa</a></li>
-              <li><a href="#">La educación y cuidado del medio ambiente</a></li>
-              <li><a href="#">Convive en áreas verdes</a></li>
-              <li><a href="#">Nueva ciudad ecológica en China</a></li>
-              <li><a href="#">Diseño de centros comerciales con paredes verdes</a></li>
-              <li><a href="#">Participa y plantea tu propuesta</a></li>
-              <li><a href="#">Vacantes en el área ecologica</a></li>
-              </ul>
-          </div>
-        </article>
-        <article class="grid_4">
-          <div class="indent-left2">
-            <h3>Conoce proyectos por el mundo</h3>
-            <figure class="prev-indent-bot"><img src="images/page1-img10.jpg" alt=""></figure>
-            <a class="link-2" href="#">Las mejores estructuras arquitectonicas con gran aportación de urbanización ecológica.</a> </div>
-        </article>
-      </div>
-    </div>
-  </div>
-</section>
-<!--==============================footer=================================-->
-<footer>
-  <div class="main">
-    <div class="aligncenter">
-      <p class="p0"> ArqyEco</p>
-      Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
-      <a href="#" >propuestasarqyeco@gmail.com</a>
-      <img src="images/maps-and-flags.png" width="10dp" height="15dp">
-     <a>Maravatio, Michoacan</a></div>
-  </div>
-</footer><script type="text/javascript">Cufon.now();</script>
-<script type="text/javascript">
-$(window).load(function () {
-    $('.slider')._TMS({
-        duration: 800,
-        easing: 'easeOutQuad',
-        preset: 'diagonalExpand',
-        slideshow: 5000
-    })
-})
-</script>
-</body>
-</html>
+							</ul>
+						</li> 
+						<li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
+						<li><a href="propuestas.blade.php">Propuestas</a></li>
+						<li><a href="ventajas.blade.php">Ventajas</a></li>
+						<li><a href="contacto.blade.php">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="row-bot">
+			<div class="row-bot-bg">
+				<div class="main">
+					<div class="wrapper p6">
+						<h1><a href="index.html">Propuestas de arquitectura y urbanización ecologica</a></h1>
+						<h2>Conocenos!</h2>
+					</div>
+					<div class="slider-wrapper">
+						<div class="slider">
+							<ul class="items">
+								<li> <img src="images/slider1.jpg" alt=""> </li>
+								<li> <img src="images/slider2.jpg" alt=""> </li>
+								<li> <img src="images/slider3.jpg" alt=""> </li>            
+							</ul>
+						</div>
+					</div>
+					<div class="wrapper">
+						<figure class="img-indent2"><img src="images/page1-img1.png" alt=""></figure>
+						<div class="extra-wrap"> <strong class="text-1">El calentamiento global es uno (y no el más grave) de los principales problemas ecológicos. Es importante señalar la degradación de hábitats por contaminación, urbanismo y la problemática con las llamadas “especies invasoras”. Porello ayudemos nuestro planeta Tierra comenzando por nuestro hogar
+							<br>  
+						"La naturaleza se hace paisaje cuando el hombre la enmarca"</strong> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--==============================aside================================-->
+	<aside>
+		<div class="carousel"> <a class="prev"></a> <a class="next"></a>
+			<div class="carousel-container">
+				<h3>¡Bienvenido!</h3>
+				<div class="jCarouselLite">
+					<ul class="carousel-list">
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img2.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img3.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img4.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img5.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img6.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img7.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img8.jpg" alt="" /></a> </div>
+						</li>
+						<li>
+							<div class="img-border"> <a href="#"><img src="images/page1-img9.jpg" alt="" /></a> </div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</aside>
+	<!--==============================content================================-->
+	<section id="content">
+		<div class="main">
+			<div class="container_12">
+				<div class="wrapper">
+					<article class="grid_4">
+						<h3 class="prev-indent-bot">Arquitectura y ecologia</h3>
+						<p class="img-indent-bot">«Yo hago lo que usted no puede, y usted hace lo que yo no puedo. Juntos podemos hacer grandes cosas». <br> Madre Teresa de Calcuta.</p>
+						<a class="button" href="#"><span>Conocer más</span></a> </article>
+						<article class="grid_4">
+							<div class="indent-left">
+								<h3 class="p1">Articulos relevantes</h3>
+								<ul class="list-1">
+									<li><a href="#">Proyecto ecologicó en América</a></li>
+									<li><a href="#">Implementa áreas verdes desde casa</a></li>
+									<li><a href="#">La educación y cuidado del medio ambiente</a></li>
+									<li><a href="#">Convive en áreas verdes</a></li>
+									<li><a href="#">Nueva ciudad ecológica en China</a></li>
+									<li><a href="#">Diseño de centros comerciales con paredes verdes</a></li>
+									<li><a href="#">Participa y plantea tu propuesta</a></li>
+									<li><a href="#">Vacantes en el área ecologica</a></li>
+								</ul>
+							</div>
+						</article>
+						<article class="grid_4">
+							<div class="indent-left2">
+								<h3>Conoce proyectos por el mundo</h3>
+								<figure class="prev-indent-bot"><img src="images/page1-img10.jpg" alt=""></figure>
+								<a class="link-2" href="#">Las mejores estructuras arquitectonicas con gran aportación de urbanización ecológica.</a> </div>
+							</article>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--==============================footer=================================-->
+			<footer>
+				<div class="main">
+					<div class="aligncenter">
+						<p class="p0"> ArqyEco</p>
+						Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
+						<a href="#" >propuestasarqyeco@gmail.com</a>
+						<img src="images/maps-and-flags.png" width="10dp" height="15dp">
+						<a>Maravatio, Michoacan</a></div>
+					</div>
+				</footer><script type="text/javascript">Cufon.now();</script>
+				<script type="text/javascript">
+					$(window).load(function () {
+						$('.slider')._TMS({
+							duration: 800,
+							easing: 'easeOutQuad',
+							preset: 'diagonalExpand',
+							slideshow: 5000
+						})
+					})
+				</script>
+			</body>
+			</html>
