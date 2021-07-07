7ac04e32be1bdbc4091a5b1d48fe48afd993a950
Gabii Albino <gabiialdacogarcia16@gmail.com>
2021-07-06 23:55:27 -0500

    se estructuro el archivo principios

--- public/principios.blade.php
+++ public/principios.blade.php
@@ -1,64 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Arquitectura y urbanización ecológica</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
-<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
-<script src="js/cufon-yui.js" type="text/javascript"></script>
-<script src="js/cufon-replace.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
-<script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
-<script src="js/FF-cash.js" type="text/javascript"></script>
+  <title>Arquitectura y urbanización ecológica</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+  <script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+  <script src="js/cufon-yui.js" type="text/javascript"></script>
+  <script src="js/cufon-replace.js" type="text/javascript"></script>
+  <script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
+  <script src="js/Kozuka_Gothic_Pro_OpenType_500.font.js" type="text/javascript"></script>
+  <script src="js/FF-cash.js" type="text/javascript"></script>
 <!--[if lt IE 9]>
 <script type="text/javascript" src="js/html5.js"></script>
 <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
 <![endif]-->
 </head>
 <body id="page3">
-<!--==============================header=================================-->
-<header>
-  <div class="menu-row">
-    <div class="main">
-      <nav class="wrapper">
-        <ul class="menu">
-          <li><a class="active" href="index.html">Inicio</a></li>
-          <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
-          <li><a href="#">Arquitectura y urbanización ecologica</a>
-                                                        <ul>
-                                                            <li><a href="arqyeco.blade.php">¿Que es?</a></li>
-                                                            <li><a href="principios.blade.php">Principio</a></li>
-                                                            <li><a href="empresas.blade.php">Empresas</a></li>
-
-                                                        </ul>
-                                                    </li> 
-          <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
-          <li><a href="propuestas.blade.php">Propuestas</a></li>
-          <li><a href="ventajas.blade.php">Ventajas</a></li>
-          <li><a href="contacto.blade.php">Contacto</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <div class="row-bot">
-    <div class="row-bot-bg">
+  <!--==============================header=================================-->
+  <header>
+    <div class="menu-row">
       <div class="main">
-        <div class="wrapper">
-          <h1><a href="index.html">Inicio</a></h1>
+        <nav class="wrapper">
+          <ul class="menu">
+            <li><a class="active" href="index.html">Inicio</a></li>
+            <li><a href="quienessomos.blade.php">¿Quiénes somos?</a></li>
+            <li><a href="#">Arquitectura y urbanización ecologica</a>
+              <ul>
+                <li><a href="arqyeco.blade.php">¿Que es?</a></li>
+                <li><a href="principios.blade.php">Principio</a></li>
+                <li><a href="empresas.blade.php">Empresas</a></li>
+
+              </ul>
+            </li> 
+            <li><a href="aplicaccion.blade.php">Aplicación en el mundo</a></li>
+            <li><a href="propuestas.blade.php">Propuestas</a></li>
+            <li><a href="ventajas.blade.php">Ventajas</a></li>
+            <li><a href="contacto.blade.php">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="row-bot">
+      <div class="row-bot-bg">
+        <div class="main">
+          <div class="wrapper">
+            <h1><a href="index.html">Inicio</a></h1>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</header>
-<!--==============================content================================-->
-<section id="content">
-  <div class="main">
-    <div class="container_12">
-      <div class="wrapper">
-        <article class="grid_3">
-          <h3 class="p1">Articulos relevantes</h3>
+  </header>
+  <!--==============================content================================-->
+  <section id="content">
+    <div class="main">
+      <div class="container_12">
+        <div class="wrapper">
+          <article class="grid_3">
+            <h3 class="p1">Articulos relevantes</h3>
             <ul class="list-1">
               <li><a href="#">Proyecto ecologicó en América</a></li>
               <li><a href="#">Implementa áreas verdes desde casa</a></li>
@@ -68,76 +68,76 @@
               <li><a href="#">Diseño de centros comerciales con paredes verdes</a></li>
               <li><a href="#">Participa y plantea tu propuesta</a></li>
               <li><a href="#">Vacantes en el área ecologica</a></li>
-              </ul>
+            </ul>
           </ul>
         </article>
         <article class="grid_9">
           <FONT color="red"><h2>Principios</h2></FONT>
           <div class="wrapper indent-bot">
 
-<h3>Los principios son los siguientes:</h3>
+            <h3>Los principios son los siguientes:</h3>
 
-<h3>1 Gestion</h3>
+            <h3>1 Gestion</h3>
 
-<p>Desde el diseño se orienta a la sustentabilidad. Se planifica para que no exista impactos ambientales o en su caso que si exista, crear medidas de mitigación.</p>
+            <p>Desde el diseño se orienta a la sustentabilidad. Se planifica para que no exista impactos ambientales o en su caso que si exista, crear medidas de mitigación.</p>
 
-<h3>2 Salud y bienestar</h3>
+            <h3>2 Salud y bienestar</h3>
 
-<p>Se orienta a espacios con confort  térmico, acústico y lumínico,  creados con elementos de la naturaleza para los ocupantes de la construcción.</p>
+            <p>Se orienta a espacios con confort  térmico, acústico y lumínico,  creados con elementos de la naturaleza para los ocupantes de la construcción.</p>
 
-<h3>3 Energia</h3>
+            <h3>3 Energia</h3>
 
-<p>Mediante la eficiencia energética, el uso de energías renovables y la optimización de iluminación y ventilación natural se reduce las emisiones de gases invernadero. </p>
+            <p>Mediante la eficiencia energética, el uso de energías renovables y la optimización de iluminación y ventilación natural se reduce las emisiones de gases invernadero. </p>
 
-<h3>4 Transporte</h3>
+            <h3>4 Transporte</h3>
 
-<p>Se fomenta el uso de transporte ecológicos como la bicicleta o a pie, dando infraestructura adecuada para motivar a las personas al uso de transporte ecológico.</p>
+            <p>Se fomenta el uso de transporte ecológicos como la bicicleta o a pie, dando infraestructura adecuada para motivar a las personas al uso de transporte ecológico.</p>
 
-<h3>5 Agua</h3>
+            <h3>5 Agua</h3>
 
-<p>Se busca reducir el consumo del agua mediante equipos ahorrador (WC ecológicos, llaves ecológicas, etc), el uso de lluvia, la reutilización del agua y el uso de plantas de tratamiento de agua.</p>
+            <p>Se busca reducir el consumo del agua mediante equipos ahorrador (WC ecológicos, llaves ecológicas, etc), el uso de lluvia, la reutilización del agua y el uso de plantas de tratamiento de agua.</p>
 
-<h3>6 Materiales</h3>
+            <h3>6 Materiales</h3>
 
-<p>Se busca construir con materiales con origen sustentables, reciclados, renovables y locales. Construir con materiales de buena calidad para que duren mucho tiempo.  El uso de materias locales reduce en el tiempo de transporte, se reduce el combustible y la contaminación. </p>
-
-
-
-<h3>7 Residuos</h3>
-
-<p>El  objetivo de un plan de manejo de residuos se minimiza la generación y se maximiza la valorización de residuos sólidos urbanos, residuos de manejo especial y residuos peligrosos específicos, bajo criterios de eficiencia ambiental.</p>
-
-<h3>8 Uso de suelo</h3>
-
-<p>El propósito es evitar el cambio de uso de suelos naturales a suelos urbanos. Se propone el reuso de edificios y espacios urbanos abandonados. También se regula el excesivo crecimiento de las ciudades a las periferias. </p>
-
-<h3>9 Contaminacion</h3>
-
-<p>Se propone reduce y evitar la contaminación desde la etapa de construcción y la etapa de ocupación del sitio. Con distintas medidas se debe reducir la contaminación de agua, aire y de los suelos. También proteger la biodiversidad.</p>
-
-<h3>10 Innovacion</h3>
-
-<p>Explorar en la investigación en el ámbito de la sustentabilidad para buscar mejoras en las técnicas y productos sustentables.
+            <p>Se busca construir con materiales con origen sustentables, reciclados, renovables y locales. Construir con materiales de buena calidad para que duren mucho tiempo.  El uso de materias locales reduce en el tiempo de transporte, se reduce el combustible y la contaminación. </p>
 
 
-</p>
+
+            <h3>7 Residuos</h3>
+
+            <p>El  objetivo de un plan de manejo de residuos se minimiza la generación y se maximiza la valorización de residuos sólidos urbanos, residuos de manejo especial y residuos peligrosos específicos, bajo criterios de eficiencia ambiental.</p>
+
+            <h3>8 Uso de suelo</h3>
+
+            <p>El propósito es evitar el cambio de uso de suelos naturales a suelos urbanos. Se propone el reuso de edificios y espacios urbanos abandonados. También se regula el excesivo crecimiento de las ciudades a las periferias. </p>
+
+            <h3>9 Contaminacion</h3>
+
+            <p>Se propone reduce y evitar la contaminación desde la etapa de construcción y la etapa de ocupación del sitio. Con distintas medidas se debe reducir la contaminación de agua, aire y de los suelos. También proteger la biodiversidad.</p>
+
+            <h3>10 Innovacion</h3>
+
+            <p>Explorar en la investigación en el ámbito de la sustentabilidad para buscar mejoras en las técnicas y productos sustentables.
+
+
+            </p>
             
-        </article>
+          </article>
+        </div>
       </div>
     </div>
-  </div>
-</section>
-<!--==============================footer=================================-->
-<footer>
-  <div class="main">
-    <div class="aligncenter">
-      <p class="p0"> ArqyEco</p>
-      Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
-      <a href="#" >propuestasarqyeco@gmail.com</a>
-      <img src="images/maps-and-flags.png" width="10dp" height="15dp">
-     <a>Maravatio, Michoacan</a></div>
-  </div>
-</footer>
-<script type="text/javascript">Cufon.now();</script>
-</body>
-</html>
+  </section>
+  <!--==============================footer=================================-->
+  <footer>
+    <div class="main">
+      <div class="aligncenter">
+        <p class="p0"> ArqyEco</p>
+        Arquitectura y urbanización ecológica <a target="_blank" href="http://www.templatemonster.com/">arqyeco.com</a><p class="p0" align="left"> </p>
+        <a href="#" >propuestasarqyeco@gmail.com</a>
+        <img src="images/maps-and-flags.png" width="10dp" height="15dp">
+        <a>Maravatio, Michoacan</a></div>
+      </div>
+    </footer>
+    <script type="text/javascript">Cufon.now();</script>
+  </body>
+  </html>
